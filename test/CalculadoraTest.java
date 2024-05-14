af302c3047b728e2299187947ef837c75e2368da
Angel <marco.4ngel@gmail.com>
2024-05-14 22:42:02 +0200

    commit

--- test/CalculadoraTest.java
+++ test/CalculadoraTest.java
@@ -3,23 +3,28 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class CalculadoraTest {
-
-
     @Test
-    public void testSuma() {
-        Double resultado = Calculadora.suma(13.5,19.5);
-        assertEquals(resultado,33);
+    void testSuma() {
+        double resultado = Calculadora.suma(19.5, 10.5);
+        assertEquals(30, resultado);
     }
 
     @Test
-    public void testResta() {
+    void testResta() {
+        double resultado = Calculadora.resta(0, 2);
+        assertEquals(-2, resultado);
     }
 
     @Test
-    public void testMultiplicacion() {
+    void testMultiplicacion() {
+        double resultado = Calculadora.multiplicacion(45, 3);
+        assertEquals(135, resultado);
     }
 
     @Test
-    public void testDivision() {
+    void testDivision() {
+        double resultado = Calculadora.division(84, 2);
+        assertEquals(42, resultado);
     }
 }
+
